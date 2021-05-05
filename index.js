d6e88a6caf35c0a237c2c7be8eff31b70128abfb
Leon Fan <leonkind@gmail.com>
2021-05-04 20:36:04 -0500

    commit tree 717361c062c2ab105935ba4e84099f76eb65733a

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then((booksArray) => {
+    renderBooks(booksArray)
+    })
 }
+
+const newArray = []
+
+function sumBookPages(books) {
+  
+  books.forEach(book => {
+    newArray.push(parseInt(book.numberOfPages))
+    return newArray
+  })
+}
+
+const summer = (accumulator, value) => {
+  return accumulator + value
+}
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+   });
 }
 
+// function renderBooks(books) {
+//   const main = document.querySelector('main')
+//   const bookList = document.createElement('ul')
+//   main.append(bookList)
+  
+//     const listItem = document.createElement('li')
+//     listItem.innerText = books.name;
+//     bookList.append(listItem)
+  
+// }
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+   fetchBooks()
+})
